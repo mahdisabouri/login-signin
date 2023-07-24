d196bfd75ba1446295b170542c25e05772a40d8a
mahdisabouri <133112639+mahdisabouri@users.noreply.github.com>
2023-07-24 19:27:04 +0330

    create login & sigin ,it's elementary

---
+++ style.js
@@ -0,0 +1,13 @@
+function signup() {
+
+    document.getElementsByClassName("signup").href="signup-1";
+    
+}
+
+
+function login() {
+
+    document.getElementsByClassName("login").href="login-1";
+    
+
+}
